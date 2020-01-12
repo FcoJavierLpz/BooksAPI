b6bd0a02e7ca61e99ed68baa8346e6c177cb7833
Francisco J. López <fcojavierlpz91@gmail.com>
2020-01-11 18:46:10 -0600

    Base components are added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorComponent } from './author/author.component';
+import { BooksComponent } from './books/books.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    AuthorsComponent,
+    AuthorComponent,
+    BooksComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
