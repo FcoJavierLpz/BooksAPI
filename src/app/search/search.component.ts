c8b8c0191dcdf584a3f5158b3157b8cd90d39246
Francisco J. López <fcojavierlpz91@gmail.com>
2020-01-26 00:06:46 -0600

    Adding search filter

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
+  @Input() searchText = '';
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
